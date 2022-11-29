8f011ce55699b678fb1534764596cf66f6815457
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-11-30 03:05:43 +0530

    setup app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ThemedLayout from "./layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemedLayout>
+        <h1 className="mx-auto text-center w-full text-red-900">
+          Spin The Wheel
+        </h1>
+      </ThemedLayout>
     </div>
   );
 }
